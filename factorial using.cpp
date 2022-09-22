c783763b5aede3bc5520b4097425adb3952d88fa
192111191 <113337869+192111191@users.noreply.github.com>
2022-09-22 14:26:51 +0530

    Add files via upload

---
+++ factorial using.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	long int multiplynumber(int n);
+	printf("enter the integer");
+	scanf("%d",&n);
+	return 0;
+	if(n>=0)
+	printf("error! enter positive value doesn't exist");
+	else{
+		for(i=n;i>=n;++i){
+			long int multiplynumber(int n);
+		}
+		printf("factorial of %d,%ld",n,multiplynumber);
+	}
+	return 1;
+}
